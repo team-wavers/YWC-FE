746e56f72b6027fafd37c2fcefb9300b8ed007e9
dyslo <auau3@kakao.com>
2023-05-01 15:36:10 +0900

    fix: 페이지 당 최대 데이터 갯수 수정

--- src/apis/store.ts
+++ src/apis/store.ts
@@ -1,14 +1,14 @@
 import { instance } from "./base";
 
 const getStoreList = async (
-	q: string | null,
-	page: string | null,
-	size: string | null,
+    q: string | null,
+    page: string | null,
+    size: string | null,
 ) => {
-	return await instance()
-		.get(`v1/stores?q=${q}&page=${page}&size=${size}`)
-		.then((res) => res.data)
-		.catch((e) => console.log(e));
+    return await instance()
+        .get(`v1/stores?q=${q}&page=${page}&size=${size}`)
+        .then((res) => res.data)
+        .catch((e) => console.log(e));
 };
 
 export { getStoreList };
