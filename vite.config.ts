ee7d09c0372c15ee16c394785d5e01e295e6fd37
dyslo <auau3@kakao.com>
2023-06-07 01:11:03 +0900

    chore: 각 환경 별 포트번호 설정 - dev: 3001, prod: 3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-});
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    return defineConfig({
+        plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+        server: {
+            port: Number(process.env.VITE_PORT),
+        },
+        build: {
+            minify: "terser",
+            terserOptions: {
+                compress: {
+                    drop_console: true,
+                    drop_debugger: true,
+                },
+            },
+        },
+    });
+};
