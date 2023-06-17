412096b339fc8967c9694d7cae5175447ae5de74
dyslo <auau3@kakao.com>
2023-06-17 12:31:36 +0900

    chore: test 환경 설정 - test를 위한 환경 설정 (libs, config, ...) - based on vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { loadEnv, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
