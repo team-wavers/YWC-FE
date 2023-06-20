48c82848b85e4816629b13d56da4c716b343184f
gpdlsrhd <gpdlsrhd@naver.com>
2023-06-20 17:03:23 +0900

    feat: 지역 필터링 api 수정 - api에 city 필터링 추가 - 지역 리스트 상수로 저장

--- src/apis/store.ts
+++ src/apis/store.ts
@@ -4,10 +4,17 @@ const getStoreList = async (
     q: string | null,
     page: string | null,
     size: number | null,
+    city: string | null,
 ) => {
-    return await instance().get(
-        `v1/stores?q=${q}&page=${page}&size=${String(size)}`,
-    );
+    if (city) {
+        return await instance().get(
+            `v1/stores?q=${q}&page=${page}&size=${String(size)}&city=${city}`,
+        );
+    } else {
+        return await instance().get(
+            `v1/stores?q=${q}&page=${page}&size=${String(size)}`,
+        );
+    }
 };
 
 const getStoreListByCoords = async (
--- src/hooks/queries/useStoreList.ts
+++ src/hooks/queries/useStoreList.ts
@@ -3,14 +3,19 @@ import { useQuery } from "@tanstack/react-query";
 import { getStoreList } from "@apis/store";
 import { AxiosError, AxiosResponse } from "axios";
 
-export const useStoreList = (q: string, page: number, size = 5) => {
+export const useStoreList = (
+    q: string,
+    page: number,
+    size = 5,
+    city: string | null,
+) => {
     return useQuery<
         Promise<AxiosResponse<responseType>>,
         AxiosError,
         responseType,
         [string, string, number]
     >(["ywc.storeList", q, page], async () => {
-        const request = await getStoreList(q, String(page), size);
+        const request = await getStoreList(q, String(page), size, city);
         return request;
     });
 };
