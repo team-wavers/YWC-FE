3922dc5757c985b197b8e11a29cca9c1fcb929b9
dyslo <auau3@kakao.com>
2023-06-23 19:52:19 +0900

    fix: 지도 내 검색 결과 하이라이트 마커 추가 - 검색 결과 클릭시 해당 장소 하이라이팅하는 마커 추가

--- src/components/Map/MapContainer.tsx
+++ src/components/Map/MapContainer.tsx
@@ -9,16 +9,17 @@ import { Listener, Marker, NaverMap, useNavermaps } from "react-naver-maps";
 import { IStores, IStore } from "@_types/store";
 import { coordType } from "@_types/coord";
 import { useStoreList } from "@hooks/queries/useStoreList";
-import PlaceInformation from "./PlaceInformation";
 import styled from "styled-components";
-import MapSearch from "./MapSearch";
 import { ReactComponent as CenterIcon } from "@assets/icons/location-icon.svg";
 import { ReactComponent as HomeIcon } from "@assets/icons/home-icon.svg";
 import { ReactComponent as CloseIcon } from "@assets/icons/close-icon.svg";
+import { ReactComponent as RefreshIcon } from "@assets/icons/refresh-icon.svg";
 import useOverlapMarkers from "@hooks/useOverlapMarkers";
 import { useNavigate } from "react-router-dom";
 import MapSearchList from "./MapSearchList";
 import MapSearchItem from "./MapSearchItem";
+import MapSearch from "./MapSearch";
+import PlaceInformation from "./PlaceInformation";
 
 type coordListType = {
     coord: coordType;
@@ -32,9 +33,10 @@ type MapContainerPropsType = {
     markers: IStores;
     onCenterChanged: (e: naver.maps.Coord) => void;
     onZoomChanged: (e: number) => void;
-    refreshBtn: React.ReactNode;
     coordList: coordListType;
     setCoordList: React.Dispatch<SetStateAction<coordListType>>;
+    refreshHandler: () => void;
+    isChanged: boolean;
 };
 
 const MapContainer = ({
@@ -42,10 +44,11 @@ const MapContainer = ({
     markers,
     onCenterChanged,
     onZoomChanged,
-    refreshBtn,
     clientCoord,
     coordList,
     setCoordList,
+    refreshHandler,
+    isChanged,
 }: MapContainerPropsType) => {
     const navermap = useNavermaps();
     const ovMarkers = useOverlapMarkers(markers);
@@ -53,7 +56,7 @@ const MapContainer = ({
     const inputRef = useRef<HTMLInputElement>(null);
     const nav = useNavigate();
     const [searchQuery, setSearchQuery] = useState<string>("");
-    const { status, data, refetch } = useStoreList(searchQuery, 1, 5); // 최상위 검색결과 5개만 가져옴
+    const { status, data, refetch } = useStoreList(searchQuery, 1, 5);
 
     const [info, setInfo] = useState<IStore>({
         _id: 0,
@@ -64,6 +67,7 @@ const MapContainer = ({
     });
     const [ovPlaceList, setOvPlaceList] = useState<IStores>([]);
     const [isVisible, setIsVisible] = useState<boolean>(false);
+    const [HLMarker, setHLMarker] = useState<IStore | null>(null);
 
     useEffect(() => {
         setOvPlaceList([]);
@@ -88,13 +92,22 @@ const MapContainer = ({
         }
     };
 
-    const clickHandler = (coord: coordType) => {
-        mapRef.current &&
-            mapRef.current.panTo(new navermap.LatLng(coord.lat, coord.lng));
-        setCoordList({
-            ...coordList,
-            coord: { result: "success", lat: coord.lat, lng: coord.lng },
-        });
+    useEffect(() => {
+        console.log(mapRef.current);
+    }, [mapRef.current]);
+
+    const clickHandler = (storeData: IStore) => {
+        const coord = { lat: storeData.latitude, lng: storeData.longitude };
+        if (coord.lat && coord.lng) {
+            mapRef.current &&
+                mapRef.current.panTo(new navermap.LatLng(coord.lat, coord.lng));
+            setCoordList({
+                ...coordList,
+                coord: { result: "success", lat: coord.lat, lng: coord.lng },
+                temp: { result: "success", lat: coord.lat, lng: coord.lng },
+            });
+        }
+        setHLMarker(storeData);
     };
 
     return (
@@ -131,25 +144,41 @@ const MapContainer = ({
                 {markers ? (
                     markers
                         .filter((store) => !ovMarkers.flat().includes(store))
-                        .map((e) => (
-                            <Marker
-                                key={e._id}
-                                position={
-                                    new navermap.LatLng(
-                                        Number(e.latitude),
-                                        Number(e.longitude),
-                                    )
-                                }
-                                onClick={() => setInfo(e)}
-                                icon={{
-                                    content: `<svg height="24" version="1.1" width="32" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><g transform="translate(0 -1028.4)"><path d="m12.031 1030.4c-3.8657 0-6.9998 3.1-6.9998 7 0 1.3 0.4017 2.6 1.0938 3.7 0.0334 0.1 0.059 0.1 0.0938 0.2l4.3432 8c0.204 0.6 0.782 1.1 1.438 1.1s1.202-0.5 1.406-1.1l4.844-8.7c0.499-1 0.781-2.1 0.781-3.2 0-3.9-3.134-7-7-7zm-0.031 3.9c1.933 0 3.5 1.6 3.5 3.5 0 2-1.567 3.5-3.5 3.5s-3.5-1.5-3.5-3.5c0-1.9 1.567-3.5 3.5-3.5z" fill="#2b78c0"/><path d="m12.031 1.0312c-3.8657 0-6.9998 3.134-6.9998 7 0 1.383 0.4017 2.6648 1.0938 3.7498 0.0334 0.053 0.059 0.105 0.0938 0.157l4.3432 8.062c0.204 0.586 0.782 1.031 1.438 1.031s1.202-0.445 1.406-1.031l4.844-8.75c0.499-0.963 0.781-2.06 0.781-3.2188 0-3.866-3.134-7-7-7zm-0.031 3.9688c1.933 0 3.5 1.567 3.5 3.5s-1.567 3.5-3.5 3.5-3.5-1.567-3.5-3.5 1.567-3.5 3.5-3.5z" fill="#309adb" transform="translate(0 1028.4)"/></g></svg>`,
-                                    size: new naver.maps.Size(32, 24),
-                                    anchor: new naver.maps.Point(16, 24),
-                                }}
-                            />
-                        ))
+                        .map((e) => {
+                            if (HLMarker && HLMarker._id === e._id)
+                                return <></>;
+                            return (
+                                <Marker
+                                    key={e._id}
+                                    position={
+                                        new navermap.LatLng(
+                                            Number(e.latitude),
+                                            Number(e.longitude),
+                                        )
+                                    }
+                                    onClick={() => setInfo(e)}
+                                    icon={{
+                                        content: `<svg height="24" version="1.1" width="32" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><g transform="translate(0 -1028.4)"><path d="m12.031 1030.4c-3.8657 0-6.9998 3.1-6.9998 7 0 1.3 0.4017 2.6 1.0938 3.7 0.0334 0.1 0.059 0.1 0.0938 0.2l4.3432 8c0.204 0.6 0.782 1.1 1.438 1.1s1.202-0.5 1.406-1.1l4.844-8.7c0.499-1 0.781-2.1 0.781-3.2 0-3.9-3.134-7-7-7zm-0.031 3.9c1.933 0 3.5 1.6 3.5 3.5 0 2-1.567 3.5-3.5 3.5s-3.5-1.5-3.5-3.5c0-1.9 1.567-3.5 3.5-3.5z" fill="#2b78c0"/><path d="m12.031 1.0312c-3.8657 0-6.9998 3.134-6.9998 7 0 1.383 0.4017 2.6648 1.0938 3.7498 0.0334 0.053 0.059 0.105 0.0938 0.157l4.3432 8.062c0.204 0.586 0.782 1.031 1.438 1.031s1.202-0.445 1.406-1.031l4.844-8.75c0.499-0.963 0.781-2.06 0.781-3.2188 0-3.866-3.134-7-7-7zm-0.031 3.9688c1.933 0 3.5 1.567 3.5 3.5s-1.567 3.5-3.5 3.5-3.5-1.567-3.5-3.5 1.567-3.5 3.5-3.5z" fill="#309adb" transform="translate(0 1028.4)"/></g></svg>`,
+                                        size: new naver.maps.Size(32, 24),
+                                        anchor: new naver.maps.Point(16, 24),
+                                    }}
+                                />
+                            );
+                        })
                 ) : (
                     <></>
+                )}
+                {HLMarker && (
+                    <Marker
+                        key={HLMarker._id}
+                        position={
+                            new navermap.LatLng(
+                                Number(HLMarker.latitude),
+                                Number(HLMarker.longitude),
+                            )
+                        }
+                        onClick={() => setInfo(HLMarker)}
+                    />
                 )}
                 {ovMarkers ? (
                     ovMarkers.map((e) => (
@@ -193,7 +222,17 @@ const MapContainer = ({
                     </Suspense>
                 )}
                 <Container>
-                    {refreshBtn}
+                    {isChanged && (
+                        <RefreshButton
+                            onClick={() => {
+                                setHLMarker(null);
+                                refreshHandler();
+                            }}
+                        >
+                            <RefreshIcon width={24} />
+                            <span>현 위치에서 검색</span>
+                        </RefreshButton>
+                    )}
                     {info._id !== 0 && (
                         <PlaceInformation
                             name={info?.name}
@@ -231,11 +270,8 @@ const MapContainer = ({
                                             category={e.category}
                                             address={e.address}
                                             clickHandler={() => {
-                                                clickHandler({
-                                                    result: "success",
-                                                    lat: Number(e.latitude),
-                                                    lng: Number(e.longitude),
-                                                });
+                                                setHLMarker(null);
+                                                clickHandler(e);
                                             }}
                                         />
                                     );
@@ -361,6 +397,25 @@ const NoResult = styled.h1`
     font-weight: 500;
     color: ${({ theme }) => theme.colors.placeholder};
     text-align: center;
+`;
+
+const RefreshButton = styled.button`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    min-width: 150px;
+    height: 50px;
+    padding: 0 10px;
+    background-color: ${({ theme }) => theme.colors.pageBtnActive};
+    border: none;
+    border-radius: 50px;
+    outline: none;
+    color: ${({ theme }) => theme.colors.white};
+    font-weight: 700;
+    font-size: ${({ theme }) => theme.sizes.l};
+    box-shadow: 0px 2px 10px -2px ${({ theme }) => theme.colors.pageBtnActive};
 `;
 
 export default MapContainer;
