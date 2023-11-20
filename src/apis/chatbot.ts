d940ca8883c1054ee2d37286bb213d4df42da1a2
dyslo <auau3@kakao.com>
2023-11-20 14:09:25 +0900

    fix: 챗봇 API Endpoint 수정 - 테스트용 API Endpoint: /chatbot -> /message

--- src/apis/chatbot.ts
+++ src/apis/chatbot.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const sendMessage = async (message: string) => {
     const bodyData = `{"message": "${message}"}`;
     return await axios.post(
-        `${import.meta.env.VITE_PUBLIC_API_ENDPOINT_LOCAL}/chatbot`,
+        `${import.meta.env.VITE_PUBLIC_API_ENDPOINT}/message`,
         bodyData,
         { headers: { "Content-Type": "application/json" } },
     );
